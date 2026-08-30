518f78213fb647537af86872ec10fdd7950ae114
mael <llado.mael.33@gmail.com>
2026-08-31 01:10:29 +0200

    delete and change minor

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,14 @@
+import React from 'react'
+import Header from '../components/header'
+
 function HomePage() {
   return (
-    <main style={{
-      minHeight: '100vh',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      fontFamily: 'Arial, sans-serif',
-      background: '#f5f7fb',
-      color: '#1f2937',
-    }}>
-      <h1>Hello world</h1>
-    </main>
+    <div>
+      <Header />
+      <main>
+        Hello world !
+      </main>
+    </div>
   )
 }
 
