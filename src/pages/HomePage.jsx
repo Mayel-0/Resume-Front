00328e7bdb4cb50f6ac4dc8ee5deb83d6c8406e3
mael <llado.mael.33@gmail.com>
2026-09-04 20:57:44 +0200

    fixe deploy name

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/header'
-import Hero from '../components/Hero'
+import Hero from '../components/hero'
 import About from '../components/about'
 import Parcours from '../components/parcours'
 import Skills from '../components/skills'
